4f153aaa26821a9061529ef75aadef5ec9475ea8
quotcode <yshivam9920@gmail.com>
2022-10-01 21:59:18 +0530

    package.json and others added

--- invoice-gen-react-app/src/App.js
+++ invoice-gen-react-app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import {} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from "./components/pages/Home";
+import Navbar from './components/layout/Navbar';
+import Invoice from './components/pages/Inovice';
+import About from './components/pages/About';
+import OurServices from './components/pages/Ourservices';
+import Contact from './components/pages/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar/>
+                <Routes>
+                    <Route exact path="/" element={<Home />}></Route>
+                    <Route exact path="/about" element={<About />}></Route>
+                    <Route exact path="/services" element={<OurServices />}></Route>
+                    <Route exact path="/invoice-generate" element={<Invoice />}></Route>
+                    <Route exact path="/contact" element={<Contact />}></Route>
+                </Routes>
+
+            </div>
+        </Router>
+
+    );
 }
 
 export default App;
